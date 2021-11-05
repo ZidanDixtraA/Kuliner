5de95ab1ca32a0be2d571aee968a57af7c258882
unknown <yazidandixtraa@gmail.com>
2021-11-05 14:21:56 +0700

    Kuliner

---
+++ app/src/main/java/com/example/kuliner/Main3Activity.java
@@ -0,0 +1,21 @@
+package com.example.kuliner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class Main3Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main3);
+    }
+
+    public void last(View view) {
+        Intent intent = new Intent(Main3Activity.this, Main4Activity.class);
+        startActivity(intent);
+    }
+}
---
+++ app/src/main/java/com/example/kuliner/View.java
@@ -0,0 +1,4 @@
+package com.example.kuliner;
+
+public class View {
+}
